2434718659b8d6f1c13eed975410bf7457672559
Michael Alan Dorman <mdorman@ironicdesign.com>
2011-03-01 11:09:24 -0500

    Add documentation

--- lib/Dancer/Plugin/Facebook.pm
+++ lib/Dancer/Plugin/Facebook.pm
@@ -1,9 +1,29 @@
 package Dancer::Plugin::Facebook;
+# ABSTRACT: Plugin linking Dancer with Facebook::Graph
 
 use Dancer ':syntax';
 use Dancer::Plugin;
 
 use Facebook::Graph;
+
+=head1 SYNOPSIS
+
+  use Dancer;
+  use Dancer::Plugin::Facebook;
+
+  get '/' => sub {
+    fb->fetch ('16665510298')->{name};
+  } # returns 'perl'
+
+=head1 DESCRIPTION
+
+Dancer::Plugin::Facebook is a simple shim taking the repetitious
+scut-work out of using Facebook::Graph from within a Dancer
+application.
+
+
+
+=cut
 
 my $fb;
 my $cf;
@@ -22,5 +42,17 @@ register fb => sub {
 };
 
 register_plugin;
+
+=head1 BUGS
+
+Please report any bugs via e-mail.
+
+=head1 SEE ALSO
+
+Dancer - L<Dancer>
+
+Facebook::Graph - L<Facebook::Graph>
+
+=cut
 
 1;
