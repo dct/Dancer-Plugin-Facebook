43b04c072544945ff146da536f0e8e73a99b5929
Michael Alan Dorman <mdorman@ironicdesign.com>
2011-03-01 11:01:30 -0500

    Flesh out our plugin.
    This is really just about simplifying configuration and consistent access
    to an Facebook::Graph object.  It's not (at this point) really doing
    anything special.

--- lib/Dancer/Plugin/Facebook.pm
+++ lib/Dancer/Plugin/Facebook.pm
@@ -1,5 +1,26 @@
-use strict;
-use warnings;
 package Dancer::Plugin::Facebook;
+
+use Dancer ':syntax';
+use Dancer::Plugin;
+
+use Facebook::Graph;
+
+my $fb;
+my $cf;
+
+register fb => sub {
+    return $fb if $fb;
+    unless (defined $cf) {
+        my $conf = plugin_setting;
+        if (ref $conf->{registration} eq "HASH") {
+            $cf = plugin_setting->{registration}
+        } else {
+            $cf = {};
+        }
+    }
+    $fb = Facebook::Graph->new (%{$cf});
+};
+
+register_plugin;
 
 1;
